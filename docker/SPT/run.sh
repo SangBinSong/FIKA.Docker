c73456d9c63fe396cd9ae508e1ebae95280b2268
SangBinSong <sangbin0810@gmail.com>
2024-05-16 15:31:16 +0000

    end edit

--- docker/SPT/run.sh
+++ docker/SPT/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 LINK_ROOT_DIR="/opt/server_data"
 
@@ -9,35 +10,39 @@ LINK_DIR_USER="/opt/server/user"
 DIR_USER="$LINK_ROOT_DIR/user"
 
 # BACKENDIP="${BACKENDIP:=$(wget -qO - ipv4.icanhazip.com)}"
+BACKENDIP="${BACKENDIP:=$(bbgurl -Q http://ipv4.icanhazip.com)}"
 
-if [ ! -d "$DIR_AKI" ]; then
+if [ ! -d "$DIR_AKI" ]; then echo "Make dir Aki_data"
     mkdir $DIR_AKI;
 fi
 
-if [ ! -L "$LINK_DIR_AKI" ]; then echo "LINK_DIR_AKI"
+if [ ! -L "$LINK_DIR_AKI" ]; then echo "Make link Aki_data"
     ln -s $DIR_AKI ;
 fi
 
-if [ $(find $DIR_AKI -maxdepth 1 -type d | wc -l) -eq 1 -a -e "$LINK_DIR_AKI.tar.gz" ]; then 
+if [ $(find $DIR_AKI -maxdepth 1 -type d | wc -l) -eq 1 -a -e "$LINK_DIR_AKI.tar.gz" ]; then echo "Unzip Aki_Data"
     tar xf $LINK_DIR_AKI.tar.gz -C $LINK_ROOT_DIR
+
+    echo "Set Default Server"
     sed -i 's/\"ip\": \"127.0.0.1\"/\"ip\": \"0.0.0.0\"/g' $LINK_DIR_AKI/Server/configs/http.json
-    # sed -i "s/\"backendIp\": \"127.0.0.1\"/\"backendIp\": \"$BACKENDIP\"/g" $LINK_DIR_AKI/Server/configs/http.json
+    sed -i "s/\"backendIp\": \"127.0.0.1\"/\"backendIp\": \"$BACKENDIP\"/g" $LINK_DIR_AKI/Server/configs/http.json
     chown $(id -u):$(id -g) $LINK_ROOT_DIR/* -Rf ;
 fi
 
-if [ ! -d "$DIR_USER" ]; then
+if [ ! -d "$DIR_USER" ]; then echo "Make dir user"
     mkdir $DIR_USER ;
 fi
 
-if [ ! -L "$LINK_DIR_USER" ]; then
+if [ ! -L "$LINK_DIR_USER" ]; then echo "Make link user"
     ln -s $DIR_USER ;
 fi
 
-if [ $(find $DIR_USER -maxdepth 1 -type d | wc -l) -eq 1 -a -e "$LINK_DIR_USER.tar.gz" ]; then 
+if [ $(find $DIR_USER -maxdepth 1 -type d | wc -l) -eq 1 -a -e "$LINK_DIR_USER.tar.gz" ]; then echo "Unzip user"
     tar xf $LINK_DIR_USER.tar.gz -C $LINK_ROOT_DIR ; 
     chown $(id -u):$(id -g) $LINK_ROOT_DIR/* -Rf ;
 fi 
 
+echo "NODE_CHANNEL_FD env set"
 NODE_CHANNEL_FD=" timeout --preserve-status 40s /opt/server/Aki.Server.exe </dev/null >/dev/null 2>&1"
 
 exec "$@"
